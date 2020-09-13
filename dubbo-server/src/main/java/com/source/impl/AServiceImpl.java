7759e961adf04759000be598bcc79dd12995da43
niepu <niepu1991@163.com>
2020-09-13 22:22:05 +0800

    uml

--- dubbo-server/src/main/java/com/source/impl/AServiceImpl.java
+++ dubbo-server/src/main/java/com/source/impl/AServiceImpl.java
@@ -6,6 +6,7 @@ import com.source.AService;
  * @date:2020/6/7 18:31
  **/
 public class AServiceImpl implements AService {
+    @Override
     public String sayAHello() {
         return "com.source.impl.AAAAAAServiceImpl#hello()";
     }
--- dubbo-server/src/main/java/com/source/impl/BServiceImpl.java
+++ dubbo-server/src/main/java/com/source/impl/BServiceImpl.java
@@ -6,6 +6,7 @@ import com.source.BService;
  * @date:2020/6/7 19:28
  **/
 public class BServiceImpl implements BService {
+    @Override
     public String sayBHello(String param) {
         return "com.source.impl.BBBBBBServiceImpl#hello()" + param;
     }
